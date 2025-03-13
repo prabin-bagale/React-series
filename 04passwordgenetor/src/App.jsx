83de8edbb9b787524a68839a955c81b01856a0c8
prabin bagale <prabinbagale08@gmail.com>
2025-03-14 00:12:07 +0545

    upadte main files

---
+++ 04passwordgenetor/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [length, setlength] = useState(8)
+  const [number, setNumber] = useState(flase)
+  const [char, setChar] = useState(false)
+  const [pass, setpass] = useState("")
+
+  const passwordGenerator= ()=>{
+    
+  }
+  return (
+    <>
+      <h1 className='text-4xl  text-white'>Password Generator</h1>
+    </>
+  )
+}
+
+export default App
